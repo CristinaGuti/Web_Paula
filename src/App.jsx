ee4a60ae0babce0a16c7e65555aba313298b0e9f
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-03-29 21:09:16 +0200

    clean react app and instagram test

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Instagram from './components/Instagram/Instagram'
+
+// import AppRoutes from './routes/AppRoutes'
+
+
+const App = () => {
+
+
+  return (
+    <div className='App'>
+
+      <div className='AppRoutes'>
+        {/* <AppRoutes /> */}
+        <Instagram />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Instagram/Instagram.jsx
@@ -0,0 +1,20 @@
+const Instagram = () => {
+
+
+    return (
+        <>
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+
+            <iframe
+                src="//lightwidget.com/widgets/80993f385be250dcbdf32b1db9dc0fc7.html"
+                title="instagram"
+                className='lightwidget-widget instagram'
+                height="600"
+            >
+            </iframe>
+        </>
+    )
+}
+
+
+export default Instagram
